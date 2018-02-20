939a05b43b91c907e9c7ec121a4772e8871af8e5
aicalvopina <kyubi900@hotmail.com>
2018-02-20 07:39:03 -0500

    Eliminacion de las TABs y creacion de las actividades para cada Entidad

---
+++ app/src/main/java/ec/edu/espe/proyectofinal/Organizacion.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.proyectofinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Organizacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_organizacion);
+    }
+}
---
+++ app/src/main/java/ec/edu/espe/proyectofinal/TCiudadano.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.proyectofinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TCiudadano extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tciudadano);
+    }
+}
